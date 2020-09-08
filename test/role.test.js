fb88dd4ffd20226a5dccc0c38b543150450d456b
Phil <phil@emiphil.com>
2020-09-08 15:19:41 -0600

    add subset and batch registrations

--- test/role.test.js
+++ test/role.test.js
@@ -33,4 +33,38 @@ describe('role tests not covered in the example cases', () => {
     expect(role.resolve('action', 'resource', { p2: 'p1' })).toBeFalsy()
     expect(role.resolve('action', 'resource', { p2: 'p2' })).toBeTruthy()
   })
+
+  test('pass array/arg list to resgisterPrivilege/Privileges', () => {
+    var role = new Role('role')
+    var p1 = new Privilege('action', 'resource', 'p1')
+    var p2 = new Privilege('action', 'resource', 'p2')
+
+    role.registerPrivilege(p1)
+    role.registerPrivilege(p2)
+
+    expect(new Role('role').registerPrivilege(p1).registerPrivilege(p2)).toEqual(role)
+    expect(new Role('role').registerPrivilege(p1, p2)).toEqual(role)
+    expect(new Role('role').registerPrivileges(p1, p2)).toEqual(role)
+    expect(new Role('role').registerPrivilege([p1, p2])).toEqual(role)
+    expect(new Role('role').registerPrivileges([p1, p2])).toEqual(role)
+  })
+
+  test('get role ids', () => {
+    let role = new Role('role')
+    let p1 = new Privilege('action', 'resource', 'p1')
+    let p2 = new Privilege('action', 'resource', 'p2')
+
+    role.registerPrivileges(p1, p2)
+
+    expect(Object.keys(role.getPrivilegeIds())).toEqual(['p1', 'p2'])
+    expect(role.getPrivilegeIds(true)).toEqual(['p1', 'p2'])
+
+    let roleB = new Role('role b')
+    let p3 = new Privilege('action', 'resource', 'p3')
+    roleB.registerPrivilege(p3)
+    role.extend(roleB)
+
+    expect(role.getPrivilegeIds(true)).toEqual(['p1', 'p2', 'p3'])
+    expect(Object.keys(role.getPrivilegeIds())).toEqual(['p1', 'p2', 'p3'])
+  })
 })
